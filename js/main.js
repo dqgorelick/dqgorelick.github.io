d65e12628598a13fb6f0ab7c7b9267b52e53d1e5
Daniel Gorelick <dqgorelick@gmail.com>
2015-11-18 12:04:10 -0500

    [DG] finished adding descriptions and content

--- js/main.js
+++ js/main.js
@@ -24,7 +24,8 @@ var projects = {
 		"title": "Bostonhacks Live Page",
 		"link": "http://live.bostonhacks.io",
 		"github": "https://github.com/boston-hacks/boston-hacks.github.io",
-		"description": "Website created for the first national hackathon at Boston University in October 2015. The site acted as the main form of communication with the 400 hackers who attended.",
+		"areas": "Design and development",
+		"description": "Website created for the first national hackathon at Boston University in October 2015. The site acted as the main form of communication with the 400 hackers who attended.<br><br>Technologies:<br>Foundation<br>Parse backend<br>CSS Keyframes",
 		"images" :[
 			"../images/bostonhacks/screenshot1.png",
 			"../images/bostonhacks/screenshot2.png"
@@ -34,7 +35,8 @@ var projects = {
 		"title": "Orator WebApp",
 		"link": "./orator",
 		"github": "https://github.com/dqgorelick/Orator",
-		"description": "24-hour hackathon project project at Make BU 2014 to explore the uses of speech to text and text semantic analysis. Created a smart teleprompter that will follow the speakers progress during a speech, as well as a speech analyzing engine which will rate a speech on various metrics such as sentiment, posture, speed, and flag profanity. Also offers support different languages!<br><br>Technologies:<br>Google Voice chrome webkit<br>Semantria API<br>HTML5 Canvas<br>HTML video and microphone",
+		"areas": "Development",
+		"description": "24-hour hackathon project project at Make BU 2014 to explore the uses of speech to text and text semantic analysis. Created a smart teleprompter that will follow the speakers progress during a speech, as well as a speech analyzing engine which will rate a speech on various metrics such as sentiment, posture, speed, and flag profanity.<br><br>Technologies:<br>Google Voice chrome webkit<br>Semantria API<br>HTML5 Canvas<br>HTML video and microphone",
 		"images" :[
 			"../images/orator/screenshot1.png",
 			"../images/orator/screenshot2.png"
@@ -43,7 +45,8 @@ var projects = {
 	wikinauts : {
 		"title": "Wikinauts Android App",
 		"link": "http://wikinauts.github.io",
-		"description": "Website created for the first national hackathon at Boston University in October 2015. The site acted as the main form of communication with the 400 hackers who attended.",
+		"areas": "Design and development",
+		"description": "The mobile take on the popular ‘Wikipedia game’, where the goal is to find the shortest path between Wikipedia article pages by only clicking on the links. The application was completed as a final project in the EC327 course at Boston University. Demo and Google Play store link are on the live page.",
 		"images" :[
 			"../images/wikinauts/screenshot1.png",
 			"../images/wikinauts/screenshot2.png",
@@ -54,7 +57,8 @@ var projects = {
 	},
 	newyorktimes : {
 		"title": "Times Trailers<br>Summer 2015",
-		"description" : "Website created for the first national hackathon at Boston University in October 2015. The site acted as the main form of communication with the 400 hackers who attended.",
+		"description" : "Video editing and rendering web application created for the New York Times. Purpose of the project is to pragmatically generate video content for static articles, for the newsroom to be able to rapidly edit and publish. The web application is built using Angular.js and can render .mov, .webm, and .gif by capturing animation frames from the canvas object. <br><br>Technologies:<br>HTML5 canvas and Fabric.js library<br>Angular.js<br>Ffmpeg.js<br>Node and express",
+		"areas": "Design and development",
 		"images" :[
 			"../images/newyorktimes/example.gif",
 			"../images/newyorktimes/screenshot1.png",
@@ -66,6 +70,7 @@ var projects = {
 		"title": "Design Portfolio Concept",
 		"link" : "http://cvin519.github.io/",
 		"github" : "https://github.com/cvin519",
+		"areas": "UX and development",
 		"description" : "Visual and UX concept for a design portfolio. All pieces of work displayed are created by Calvin Chin (BU ‘16).",
 		"images" :[
 			"../images/calvin/screenshot1.png",
@@ -75,6 +80,7 @@ var projects = {
 	map : {
 		"title": "Boston University Marauder's Map",
 		"description" : "Created for the Boston University chapter of Global Water Brigades’ Harry Potter Movie Marathon event. The maps are printed on tabloid size parchment paper in color. Painted elements created by Alison Staffin (BU ‘17).",
+		"areas": "Design",
 		"images" :[
 			"../images/map/screenshot1.jpg",
 			"../images/map/screenshot2.jpg",
@@ -85,6 +91,7 @@ var projects = {
 	typespecimen : {
 		"title": "Type Specimens: Helvetica",
 		"description" : "A type specimen poster and digital material created for the font face Helvetica. The project was created during a Typography course in the College of Fine Arts at Boston University. The poster is 24 inches by 36 inches printed black and white.",
+		"areas": "Design",
 		"images" : [
 			"../images/typespecimen/screenshot1.png",
 			"../images/typespecimen/screenshot2.png",
@@ -97,6 +104,7 @@ function openModal(id){
 	$(".modal").css("display", "inherit");
 	location.hash = id;
 	(projects[id].title ? $(".title").html(projects[id].title) : $(".title").html(""));
+	(projects[id].areas ? $(".areas").html(projects[id].areas) : $(".areas").html(""));
 	(projects[id].link ? $(".html-link").html("&#8212;<a href='" + projects[id].link + "'>Live site</a>") : $(".html-link").html(""));
 	(projects[id].github ? $(".github-link").html("&#8212;<a href='" + projects[id].github + "'>Github</a>") : $(".github-link").html(""));
 	(projects[id].description ? $(".description").html(projects[id].description) : $(".description").html(""));
