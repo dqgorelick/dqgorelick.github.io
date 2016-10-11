abf7031649efd0221353b4a64fbabdf9aaadc8e3
Daniel Gorelick <dqgorelick@gmail.com>
2016-10-11 12:56:24 -0400

    WIP long form stuffs

--- src/components/longform/LongformWrapper.jsx
+++ src/components/longform/LongformWrapper.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-import { PROJECTS, ACTIVE_PROJECTS } from '../../data/projects';
+import { PROJECTS } from '../../data/projects';
 
 export default function LongformWrapper (props) {
     return (
-        <div>
+        <div className='lf-wrapper'>
             <h2>LONG FORM WRAPPER YAASSSS</h2>
         </div>
     );
